1bfa9d14f55bdee8ef4d608a7b70cdc03a46042e
Luan Alba <luan.alba2@gmail.com>
2025-07-08 19:48:54 -0300

    Criado a logica para executar a criação do user e salvar no banco

--- src/services/user/CreateUserService.ts
+++ src/services/user/CreateUserService.ts
@@ -1,3 +1,5 @@
+import prisma from "../../prisma";
+
 
 interface UserRequest {
     name: string;
@@ -7,7 +9,38 @@ interface UserRequest {
 
 class CreateUserService {
     async execute({ name, email, password }: UserRequest) {
-        return { name: name }
+        //verificar se enviou um email
+        if (!email) {
+            throw new Error("Email incorreto")
+        }
+
+        //verificar se esse email ja está cadastrado
+        const userAlreadyExists = await prisma.user.findFirst({
+            where: {
+                email: email
+            }
+        })
+
+        if (userAlreadyExists) {
+            throw new Error("Email já existe cadastrado!")
+        }
+
+        //passando os dados no método de criação
+        const user = await prisma.user.create({
+            data: {
+                name: name,
+                email: email,
+                password: password
+            },
+            //select para escolher o que é necessario devolver
+            select:{
+                id: true,
+                name: true,
+                email: true,
+            }
+        })
+
+        return user;
     }
 }
 
