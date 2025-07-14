31d1f6f37f2a99526f3fa85ae1f14c11da0c18e9
Luan Alba <luan.alba2@gmail.com>
2025-07-14 17:16:00 -0300

    classe para listar produtos por categoria

---
+++ src/services/product/ListByCategoryService.ts
@@ -0,0 +1,20 @@
+import prisma from "../../prisma";
+
+interface ProductRequest {
+    category_id: string
+}
+
+class ListByCategoryService {
+    async execute({ category_id } : ProductRequest) {
+
+        const findByCategory = await prisma.product.findMany({
+            where:{
+                category_id: category_id
+            }
+        })
+
+        return findByCategory;
+    }
+}
+
+export { ListByCategoryService }
